9f872391112d1ccf0987d31fab1c9de951b63ea2
Nadav Cohen <ncamaa@gmail.com>
2026-03-24 14:41:37 +0100

    Add GA4 measurement ID

--- src/lib/analytics.ts
+++ src/lib/analytics.ts
@@ -7,7 +7,7 @@
  *
  * Leave empty to keep analytics disabled.
  */
-export const GA_MEASUREMENT_ID = ''
+export const GA_MEASUREMENT_ID = 'G-N1NQ14Q1G6'
 export const GOOGLE_SITE_VERIFICATION = ''
 
 const GA_SCRIPT_URL = 'https://www.googletagmanager.com/gtag/js'
